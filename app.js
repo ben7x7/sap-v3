eeb87b7d2205ff6c13faf255db61e27702db6faa
benoit bessiere <benoit.bessiere@gmail.com>
2020-10-22 08:26:25 +0200

    mentions legales

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 tl.to(".text", { y: "0%", duration: 1.5, stagger: 1 });
-tl.to(".slider", { y: "-100%", duration: 1, delay: 0.5 });
+tl.to(".slider", { y: "-100%", duration: 1, delay: 0.1 });
 tl.to(".intro", { y: "-100%", duration: 0.5 }, "-=1");
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 0.3 });
-tl.fromTo(".home", { opacity: 0 }, { opacity: 1, duration: 0.3 }, "-=0.3");
+tl.fromTo(".home", { opacity: 0 }, { opacity: 1, duration: 0.3 }, "-=1");
