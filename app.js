8b5405a890be113b82d36ce0c1fc9eeb36f8d889
benoit bessiere <benoit.bessiere@gmail.com>
2020-11-21 10:04:16 +0100

    add cookies rules

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ tl.to(".slider", { y: "-100%", duration: 1, delay: 0.1 });
 tl.to(".intro", { y: "-100%", duration: 0.5 }, "-=1");
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 0.3 });
 tl.fromTo(".home", { opacity: 0 }, { opacity: 1, duration: 0.3 }, "-=1");
+
+
+  const cookiesBanner = document.querySelector('.cookies-banner');
+  const btnAccepted = document.querySelector('#accepted');
+
+  btnAccepted.addEventListener('click', () => {
+    cookiesBanner.classList.add("consentment");
+  });
+
